c7a2639ae8a621d0f2e363d193f7a989ac05760f
Zoechbauer <hans.zoechbauer@aon.at>
2020-05-24 22:12:37 +0200

    @angular/flex-layout installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,12 +12,19 @@ import { CourseDetailComponent } from './courses/course-detail/course-detail.com
 import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, CoursesComponent, CourseDetailComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    CoursesComponent,
+    CourseDetailComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
