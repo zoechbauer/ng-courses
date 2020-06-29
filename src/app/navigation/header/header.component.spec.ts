1ecf011fa6d02f7fb0a830d73c826ba8fd948f99
Zoechbauer <hans.zoechbauer@aon.at>
2020-06-29 19:26:28 +0200

    Header: specs defined & listed as pending

--- src/app/navigation/header/header.component.spec.ts
+++ src/app/navigation/header/header.component.spec.ts
@@ -1,24 +1,57 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AuthStore } from 'src/app/auth/auth.store';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { MaterialModule } from 'src/app/material.module';
 
-xdescribe('HeaderComponent', () => {
+describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let authStore: AuthStore;
 
   beforeEach(async(() => {
+    const authStoreSpy = jasmine.createSpyObj('AuthStore', ['logout']);
+
     TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-    }).compileComponents();
+      imports: [AppRoutingModule, MaterialModule],
+      providers: [{ provide: AuthStore, useValue: authStoreSpy }],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        authStore = TestBed.inject(AuthStore);
+      });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show menu item logout if user is logged in', () => {
+    pending();
+  });
+
+  it('should show menu item help if user is logged in', () => {
+    pending();
+  });
+
+  it('should show menu item login if user is logged out', () => {
+    pending();
+  });
+
+  it('should show menu item Courses if user is logged in', () => {
+    pending();
+  });
+
+  it('should show Text Admin if user is logged in as admin', () => {
+    pending();
+  });
+
+  it('should deactvate menu item Manage Courses and New Course unless user is admin', () => {
+    pending();
   });
 });
