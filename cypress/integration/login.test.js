0f45a6193e79c6d818a4700b3280dd7144dd52aa
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-07 20:46:15 +0200

    Setup for e2e-test with Cypress and Continous Integration with Travis Ci

---
+++ cypress/integration/login.test.js
@@ -0,0 +1,9 @@
+// type definitions for Cypress object "cy"
+/// <reference types="cypress" />
+
+describe('Login Page', () => {
+  it('should test cypress setup', () => {
+    expect(true).be.true;
+    expect(true).not.to.be.eq(false);
+  });
+});
